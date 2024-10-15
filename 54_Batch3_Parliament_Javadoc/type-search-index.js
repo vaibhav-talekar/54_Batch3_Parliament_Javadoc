e8f89395567aa46760e1da08cc413d3a84cc2c7c
vaibhav-talekar <mca2024.vaibhav@unigoa.ac.in>
2024-10-15 11:18:01 +0530

    Add files via upload

---
+++ 54_Batch3_Parliament_Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Parliament","l":"IndianParliament"},{"p":"Parliament","l":"loksabha"},{"p":"Parliament","l":"president"},{"p":"Parliament","l":"rajyasabha"}];updateSearchResults();
